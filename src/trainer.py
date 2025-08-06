e3e7c5b82a5f3ed2549e3aaafc024e4525615a74
Rohit <nirohit@gmail.com>
2025-08-06 21:46:50 +0100

    Ready for remote training

--- src/trainer.py
+++ src/trainer.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import json 
-from train.train_utils import finetune_helper
+from train.train_utils import finetune_helper, train_model
 from train.instruction_data import TrainData, collate, collate_v2
 from torch.utils.data import DataLoader
 from functools import partial
@@ -11,13 +11,14 @@ from modules.polymnia import Polymnia
 from tools.config import CALLIOPE_CONFIG_124M, model_configs
 from tools.load_weights import load_weights_into_gpt
 from tools.utils import calc_loss_loader, generate_text, text_to_token_ids, token_ids_to_text
+import time
 
 
 # For local vs remote training
 LOCAL_INSTRUCTIONS = "./train/data/instructions-finetune-local.json"
 REMOTE_INSTRUCTIONS = "./train/data/instructions-finetune.json"
 
-with open(LOCAL_INSTRUCTIONS,"r") as f:
+with open(REMOTE_INSTRUCTIONS,"r") as f:
     data = json.load(f)
 
 
@@ -25,6 +26,9 @@ NUM_WORKERS = 0
 BATCH_SIZE = 8
 WEIGHTS_PATH = './weights/'
 MODEL_SIZE = "774M"
+NUM_EPOCHS = 5
+EVAL_FREQ = 5
+EVAL_ITER = 5
 
 
 model_name = "gpt2-large-774M" # Change this to the desired model size
@@ -74,13 +78,34 @@ settings, params = load_settings_and_params(WEIGHTS_PATH + MODEL_SIZE)
 
 model = Polymnia(MODEL_CONFIG)
 load_weights_into_gpt(model, params)
-model.eval()
+# model.eval()
 
 
-input_data = finetune_helper(validate_data[0])
+# input_data = finetune_helper(validate_data[0])
 
 
-token_ids = generate_text(model, text_to_token_ids(input_data, tokenizer), 50, MODEL_CONFIG["context_length"], eos_id=50256)
-generate_text = token_ids_to_text(token_ids, tokenizer)
+# token_ids = generate_text(model, text_to_token_ids(input_data, tokenizer), 50, MODEL_CONFIG["context_length"], eos_id=50256)
+# generate_text = token_ids_to_text(token_ids, tokenizer)
 
-print(f"Generated text: {generate_text}")
+# print(f"Generated text: {generate_text}")
+
+
+model.to(device)
+# with torch.no_grad():
+#     train_loss = calc_loss_loader(train_loader, model, device, num_batches=5)
+#     val_loss = calc_loss_loader(val_loader, model, device, num_batches=5)
+
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=MODEL_CONFIG["learning_rate"], weight_decay=MODEL_CONFIG["weight_decay"])
+
+start_time = time.time()
+start_context = finetune_helper(validate_data[0])
+
+train_losses, val_losses, track_tokens_seen = train_model(model, train_loader, val_loader, optimizer, device, NUM_EPOCHS, EVAL_FREQ, EVAL_ITER, start_context, tokenizer)
+
+end_time = time.time()
+execution_time = (end_time - start_time) / 60
+
+
+torch.save(model.state_dict(),"weights/model-774m-al00-v1.pth")
+
